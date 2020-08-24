1f918266859ffdd0a0494fcf4e7c462701cd2319
I Komang Ariana <komang.ariana@dana.id>
2020-08-24 17:41:27 +0800

    Create header component and put this in layout component.

--- layouts/users.vue
+++ layouts/users.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
+    <Header />
     <header>
       <h1>USER SECTION</h1>
     </header>
     <Nuxt />
   </div>
 </template>
+
+<script>
+import Header from '~/components/Header'
+
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
 
 <style>
 </style>
