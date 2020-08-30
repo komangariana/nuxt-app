ea2f9ef66b2dda3565b7176c4c5bd0f3f6ad2397
I Komang Ariana <komang.ariana@dana.id>
2020-08-30 13:20:05 +0800

    Import post list component to home page and blog page.

--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,18 @@
     <section class="intro">
       <h1>Get latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        thumbnail="https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg"
-        title="Example Post 1"
-        previewText="This is the preview text 1"
-      />
-      <PostPreview
-        id="2"
-        thumbnail="https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg"
-        title="Example Post 2"
-        previewText="This is the preview text 2"
-      />
-      <PostPreview
-        id="3"
-        thumbnail="https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg"
-        title="Example Post 3"
-        previewText="This is the preview text 3"
-      />
-    </section>
+    <PostList/>
   </div>
 </template>
 
 <script>
 import PostPreview from '@/components/Posts/PostPreview'
+import PostList from '@/components/Posts/PostList'
 
 export default {
   components: {
     PostPreview,
+    PostList,
   }  
 }
 </script>
@@ -66,14 +49,5 @@ export default {
   .intro h1 {
     font-size: 2rem;
   }
-}
-
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
 }
 </style>
