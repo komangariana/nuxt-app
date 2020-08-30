e9cf2c773cba29cdae84005a9af1c69b916bfdbf
I Komang Ariana <komang.ariana@dana.id>
2020-08-30 14:01:59 +0800

    Add isAdmin data to differentiate admin page and non admin page.

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg"
       title="Example Post 1"
       previewText="This is the preview text 1"
     />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg"
       title="Example Post 2"
       previewText="This is the preview text 2"
     />
     <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg"
       title="Example Post 3"
       previewText="This is the preview text 3"
@@ -27,7 +30,13 @@ import PostPreview from '@/components/Posts/PostPreview'
 export default {
   components: {
     PostPreview,
-  }  
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    }
+  }
 }
 </script>
 
