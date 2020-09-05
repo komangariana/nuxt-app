bb0502494d25eeeb81b5d576a93de9d0f6233a2b
I Komang Ariana <km.ariana1992@gmail.com>
2020-09-05 15:38:48 +0800

    Use promise in async data.

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts"/>
   </div>
 </template>
 
@@ -10,7 +10,40 @@ import PostList from '@/components/Posts/PostList'
 export default {
   components: {
     PostList,
-  }  
+  },
+  data () {
+    return {
+      loadedPosts: [],
+    }
+  },
+  asyncData (context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ 
+          loadedPosts: [
+            { 
+              id: '1',
+              title: 'First Title',
+              thumbnail: 'https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg',
+              previewText: 'Preview Text 1'
+            },
+            { 
+              id: '2',
+              title: 'Second Title',
+              thumbnail: 'https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg',
+              previewText: 'Preview Text 2'
+            },
+          ]
+        })
+      }, 1500)
+    })
+    .then((data) => {
+      return data
+    })
+    .catch((e) => {
+      context.error(new Error())
+    })
+  },
 }
 </script>
 
