bb6020579b862c094b72862993a2d92cdeef4866
I Komang Ariana <km.ariana1992@gmail.com>
2020-09-06 14:31:01 +0800

    Fetch post data from firebase.

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex'
+import axios from 'axios'
 
 const createStore = () => {
   return new Vuex.Store({
@@ -12,28 +13,15 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          if (context.store.state.loadedPosts.length > 0) {
-            return
-          }
-          setTimeout(() => {
-            vuexContext.commit('setPosts', [
-              { 
-                id: '1',
-                title: 'First Title',
-                thumbnail: 'https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg',
-                previewText: 'Preview Text 1'
-              },
-              { 
-                id: '2',
-                title: 'Second Title',
-                thumbnail: 'https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg',
-                previewText: 'Preview Text 2'
-              },
-            ])
-            resolve()
-          }, 1500)
-        })
+        return axios.get('https://nuxt-blog-komang.firebaseio.com/posts.json')
+          .then((res) => {
+            const postsArray = []
+            for (const key in res.data) {
+              postsArray.push({ ...res.data[key], id: key })
+            }
+            vuexContext.commit('setPosts', postsArray)
+          })
+          .catch((e) => {console.error(e)})
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts)
