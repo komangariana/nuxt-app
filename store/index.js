839ccecc2a1c00ad68ac021527278bf16ed79d75
I Komang Ariana <km.ariana1992@gmail.com>
2020-09-15 22:53:14 +0800

    Add middleware authentication.

--- store/index.js
+++ store/index.js
@@ -75,6 +75,9 @@ const createStore = () => {
     getters: {
       loadedPosts(state) {
         return state.loadedPosts
+      },
+      isAuthenticated(state) {
+        return state.token != null
       }
     }
   })
