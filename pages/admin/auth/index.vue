46e2689c8881abddf6e6ce6bc93cdca43998d99a
I Komang Ariana <km.ariana1992@gmail.com>
2020-09-15 22:14:08 +0800

    Add authentication method.

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="admin-auth-page">
     <div class="auth-container">
-      <form>
-        <AppControlInput type="email">E-Mail Address</AppControlInput>
-        <AppControlInput type="password">Password</AppControlInput>
+      <form @submit.prevent="onSubmit">
+        <AppControlInput type="email" v-model="email">E-Mail Address</AppControlInput>
+        <AppControlInput type="password" v-model="password">Password</AppControlInput>
         <AppButton type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</AppButton>
         <AppButton
           type="button"
@@ -21,9 +21,28 @@ export default {
   layout: 'admin',
   data() {
     return {
-      isLogin: true
+      isLogin: true,
+      email: '',
+      password: '',
     }
-  }
+  },
+  methods: {
+    onSubmit () {
+      const { email, password } = this
+      let authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='
+      if (this.isLogin) {
+        authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='
+      }
+      authUrl += process.env.firebaseAPIKey
+      this.$axios.$post(authUrl, {
+        email,
+        password,
+        returnSecureToken: true,
+      }).then(result => {
+        console.log(result)
+      }).catch(e => console.log(e))
+    }
+  },
 }
 </script>
 
